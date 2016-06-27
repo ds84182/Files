463d57ba0f00c78c7293584278b05af3a50d0845
Dwayne Slater <ds84182@gmail.com>
2016-06-27 19:49:23 -0400

    Add dark and accent colors to STYLE

--- source/style/color.cpp
+++ source/style/color.cpp
@@ -2,3 +2,7 @@
 
 // MD Green 500
 GFX::Color Style::colorPrimary(0xFF4CAF50);
+// MD Green 700
+GFX::Color Style::colorPrimaryDark(0xFF388E3C);
+// MD Orange A400
+GFX::Color Style::colorAccent(0xFFFF9100);
--- source/style/color.hpp
+++ source/style/color.hpp
@@ -5,5 +5,7 @@
 namespace Style {
 
     extern GFX::Color colorPrimary;
+    extern GFX::Color colorPrimaryDark;
+    extern GFX::Color colorAccent;
 
 }
