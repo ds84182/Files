3e0e0c007a225526639f7bf5adedd4859d233461
Dwayne Slater <ds84182@gmail.com>
2016-06-28 12:51:43 -0400

    Add heap usage statistics to StatusBar for now

--- source/ui/elements/status_bar.cpp
+++ source/ui/elements/status_bar.cpp
@@ -1,5 +1,7 @@
 #include "status_bar.hpp"
 
+#include <malloc.h>
+
 #include <graphics/rectangle.hpp>
 #include <graphics/scissor.hpp>
 
@@ -7,9 +9,21 @@
 
 UI::Elements::StatusBarData UI::Elements::StatusBar::Data;
 
+extern u32 __ctru_heap_size;
+extern u32 __ctru_linear_heap_size;
+
 void UI::Elements::StatusBar::init() {
     bounds = Bounds(400, 24);
 }
+
+static void convertToU32(const char *str, std::u32string &dest) {
+    // Convert the given utf8 string into utf32
+    ssize_t len = utf8_to_utf32(nullptr, (const u8*)str, 0);
+    dest = std::move(std::u32string(len, '\0'));
+    utf8_to_utf32((u32*)dest.data(), (const u8*)str, dest.length());
+}
+
+char memUsageBuffer[1024] = {0};
 
 void UI::Elements::StatusBar::render(float timeDelta) {
     GFX::Scissor scissor(bounds);
@@ -22,4 +36,13 @@ void UI::Elements::StatusBar::render(float timeDelta) {
     int textHeight = font.height();
     // TODO: GFX::Color::Black
     font.drawText(Data.systemTime, bounds.right-textWidth-8, 12-textHeight/2, GFX::Color(255,255,255));
+
+    struct mallinfo mi = mallinfo();
+    sprintf(memUsageBuffer, "Linear: %d/%d; Normal: %d/%d",
+        __ctru_linear_heap_size-linearSpaceFree(), __ctru_linear_heap_size,
+        //0x600000-vramSpaceFree(), 0x600000);
+        mi.uordblks, __ctru_heap_size);
+    std::u32string memUsage;
+    convertToU32(memUsageBuffer, memUsage);
+    font.drawText(memUsage, 8, 12-textHeight/2, GFX::Color(255,255,255));
 }
