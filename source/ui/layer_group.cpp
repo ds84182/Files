5e2b79fe63dc19f3fecb19a79a410b0d84b59437
Dwayne Slater <ds84182@gmail.com>
2016-06-28 13:24:32 -0400

    UI::LayerGroup: Fix indentation, allow groups to be inserted before or after other groups

--- source/ui/layer_group.cpp
+++ source/ui/layer_group.cpp
@@ -12,54 +12,66 @@
 #include <utils/math_ext.hpp>
 
 UI::LayerGroup::LayerGroup() {
-  if (UI::Manager::RootLayerGroup == nullptr) {
-    UI::Manager::RootLayerGroup = this;
-  } else {
-    auto current = UI::Manager::RootLayerGroup;
-    while (current->next) current = current->next;
-    current->next = this;
-    prev = current;
-  }
-  printf("Layer Group %p created\n", this);
+    if (UI::Manager::RootLayerGroup == nullptr) {
+        UI::Manager::RootLayerGroup = this;
+    } else {
+        auto current = UI::Manager::RootLayerGroup;
+        while (current->next) current = current->next;
+        current->next = this;
+        prev = current;
+    }
+}
+
+UI::LayerGroup::LayerGroup(LayerGroup *relative, bool before) {
+    if (before) {
+        if (!relative->prev) {
+            UI::Manager::RootLayerGroup = this;
+        }
+        this->prev = relative->prev;
+        relative->prev = this;
+        this->next = relative;
+    } else {
+        this->next = relative->next;
+        relative->next = this;
+        this->prev = relative;
+    }
 }
 
 UI::LayerGroup::~LayerGroup() {
-  if (next) {
-    next->prev = prev;
-  }
-  if (prev) {
-    prev->next = next;
-  }
+    if (next) {
+        next->prev = prev;
+    }
+    if (prev) {
+        prev->next = next;
+    }
 
-  if (UI::Manager::RootLayerGroup == this) {
-    UI::Manager::RootLayerGroup = next;
-  }
-
-  printf("Layer Group %p deleted\n", this);
+    if (UI::Manager::RootLayerGroup == this) {
+        UI::Manager::RootLayerGroup = next;
+    }
 }
 
 void UI::LayerGroup::remove(Layer *layer) {
-  layers.erase(std::remove(layers.begin(), layers.end(), layer), layers.end());
+    layers.erase(std::remove(layers.begin(), layers.end(), layer), layers.end());
 }
 
 static C3D_Mtx compostMatrix;
 
 void UI::LayerGroup::compost(float timeDelta) {
-  if (disabled && !overrideDisableForRender) return;
+    if (disabled && !overrideDisableForRender) return;
 
-  for (auto layer : layers) {
-    if (layer->compost) {
+    for (auto layer : layers) {
+        if (layer->compost) {
 			Mtx_Ortho(&compostMatrix, 0.0, float(layer->compostFB.width), float(layer->compostFB.height), 0.0, 0.0, 1.0);
 			GFX::DrawOn(&layer->compostFB, &compostMatrix, true);
 			layer->render(timeDelta, true);
 		}
-  }
+    }
 }
 
 void UI::LayerGroup::render(float timeDelta) {
-  if (disabled && !overrideDisableForRender) return;
+    if (disabled && !overrideDisableForRender) return;
 
-  for (auto layer : layers) {
-    layer->render(timeDelta);
-  }
+    for (auto layer : layers) {
+        layer->render(timeDelta);
+    }
 }
--- source/ui/layer_group.hpp
+++ source/ui/layer_group.hpp
@@ -8,27 +8,27 @@ namespace UI {
 
 class LayerGroup {
 public:
-  LayerGroup *next = nullptr;
-  LayerGroup *prev = nullptr;
-  std::vector<Layer*> layers;
-  bool disabled = false;
-  bool overrideDisableForRender = false;
+    LayerGroup *next = nullptr;
+    LayerGroup *prev = nullptr;
+    std::vector<Layer*> layers;
+    bool disabled = false;
+    bool overrideDisableForRender = false;
 
-  LayerGroup();
+    LayerGroup();
+    LayerGroup(LayerGroup *relative, bool before = true);
+    ~LayerGroup();
 
-  ~LayerGroup();
+    void add(Layer *layer) {
+        layers.push_back(layer);
+    }
 
-  void add(Layer *layer) {
-    layers.push_back(layer);
-  }
+    void remove(Layer *layer);
 
-  void remove(Layer *layer);
+    void compost(float delta);
+    void render(float delta);
 
-  void compost(float delta);
-  void render(float delta);
-
-  std::shared_ptr<ElementBase> find(std::function<bool(std::shared_ptr<ElementBase>&)> func) {
-    if (disabled) return std::shared_ptr<ElementBase>();
+    std::shared_ptr<ElementBase> find(std::function<bool(std::shared_ptr<ElementBase>&)> func) {
+        if (disabled) return std::shared_ptr<ElementBase>();
 
 		auto layerit = layers.rbegin();
 		auto layeritend = layers.rend();
