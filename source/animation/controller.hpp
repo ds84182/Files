c79bbbc9968bfbed16d9de536d860f626158a831
Dwayne Slater <ds84182@gmail.com>
2016-07-04 10:10:01 -0400

    Animation::Controller: Allow animations to be canceled and auto remove finished animations

--- source/animation/controller.hpp
+++ source/animation/controller.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <cstdlib>
 #include <map>
 #include <string>
@@ -27,10 +28,12 @@ struct AnimationState {
 	State state;
 	bool reverse;
 	OnDone onDone;
+	std::string tag;
 	AnimationState(TimeInterpolator interpolator, Target target, float duration,
-			OnDone onDone = nullptr, float time = 0.0, State state = State::Running, bool reverse = false) :
+			OnDone onDone = nullptr, std::string tag = "", float time = 0.0,
+			State state = State::Running, bool reverse = false) :
 		interpolator(interpolator), target(target), time(time), duration(duration), state(state),
-		reverse(reverse), onDone(onDone) {}
+		reverse(reverse), onDone(onDone), tag(tag) {}
 };
 
 // Controls the interactions between time interpolators and targets.
@@ -40,7 +43,7 @@ public:
 	AnimationState &start(TimeInterpolator interpolator, Target target, float duration,
 			OnDone onDone = nullptr, std::string tag = "", float time = 0.0, State state = State::Running,
 			bool reverse = false) {
-		animations.emplace_back(interpolator, target, duration, onDone, time, state, reverse);
+		animations.emplace_back(interpolator, target, duration, onDone, tag, time, state, reverse);
 		AnimationState &anim = animations.back();
 		if (!tag.empty()) {
 			tagMap.emplace(tag, anim);
@@ -61,6 +64,13 @@ public:
 	AnimationState &getTag(std::string tag) {
 		if (!hasTag(tag)) abort();
 		return tagMap.at(tag);
+	}
+
+	void cancelTag(std::string tag) {
+		if (!hasTag(tag)) abort();
+		animations.erase(std::remove_if(animations.begin(), animations.end(), [&](auto &state) {
+			return state.tag == tag;
+		}));
 	}
 
 	void update(float delta) {
@@ -86,6 +96,11 @@ public:
 				}
 			}
 		}
+
+		if (!animations.empty())
+			animations.erase(std::remove_if(animations.begin(), animations.end(), [](auto &state) {
+				return state.state == State::Done;
+			}), animations.end());
 	}
 private:
 	std::vector<AnimationState> animations;
