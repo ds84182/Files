4b1cc4658f65514ffb97eed1fbc5277941d55840
Dwayne Slater <ds84182@gmail.com>
2016-07-04 10:14:46 -0400

    DirectoryEntryElement: use wstring instead of u32string

--- source/ui/elements/directory_entry.hpp
+++ source/ui/elements/directory_entry.hpp
@@ -17,7 +17,7 @@ struct DirectoryEntryData {
 	bool valid = false;
 	FS::Path path;
 	FS::EntryType type = FS::EntryType::None;
-	std::u32string name;
+	std::wstring name;
 
 	DirectoryEntryData() {}
 
@@ -32,7 +32,7 @@ private:
 		if (name.empty()) {
 			// Convert the given utf8 string into utf32
 			ssize_t nameLength = utf8_to_utf32(nullptr, (const u8*)path.name().c_str(), 0);
-			name = std::u32string(nameLength, '\0');
+			name = std::wstring(nameLength, '\0');
 			utf8_to_utf32((u32*)name.data(), (const u8*)path.name().c_str(), name.length());
 		}
 	}
